45bfe95e24dcb8bc9450332ae34fb99e9932fa30
ronald0204 <ronaldaguirre0204@gmail.com>
2021-12-23 11:43:18 -0500

    update the files the proyect hotels

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,10 +44,20 @@ const routes: Routes = [
     ],
   },
   // no layout views
-  { path: "profile", component: ProfileComponent },
-  { path: "landing", component: LandingComponent },
-  { path: "", component: IndexComponent },
-  { path: "**", redirectTo: "", pathMatch: "full" },
+  {
+    path: "profile", 
+    component: ProfileComponent
+  },
+  { path: "landing",
+    component: LandingComponent
+  },
+  { path: "",
+    component: IndexComponent
+  },
+  { path: "**", 
+    redirectTo: "", 
+    pathMatch: "full" 
+  }
 ];
 
 @NgModule({
